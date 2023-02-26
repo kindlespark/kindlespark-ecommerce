b94da9fe25b50488f167538ca4e541ff38ed20a3
Rakesh <jagtap.python@gmail.com>
2023-02-26 18:15:40 +0000

    modified app.py file for shop method

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 def home_page() -> str:
     print("Inside home")
     return "Hello from Vinay on the landing page v.0.0.1!"
+
+@app.route("/shop")
+def shop_page() -> str:
+    print("Method added by Rakesh Jagtap")
+    return "Hello from Rakesh the landing page v.0.0.1!"
