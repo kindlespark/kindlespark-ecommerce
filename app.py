41e3ec64aee418dfe161579576fe3a1ff439a81b
Vinay Sanyasi <vinay.sanyasi@gmail.com>
2023-03-02 23:11:53 -0700

    added build and release steps

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home_page() -> str:
     print("Inside home")
-    return "Hello from KindleSpark Team on the landing page v.0.0.1!"
+    return "Hello from KindleSpark Team on the landing page v.0.0.2!"
 
 @app.route("/shop")
 def shop_page() -> str:
